ace94af5eb005a00bb949deaccce06ff585a0446
Patrice Gill <pgill30@fb.com>
2020-07-06 12:54:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simplechat/ChatActivity.java
@@ -0,0 +1,47 @@
+package com.example.simplechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class ChatActivity extends AppCompatActivity {
+    static final String TAG = ChatActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        //User login
+        if (ParseUser.getCurrentUser() != null) { //Either start with an existing user
+            startWithCurrentUser();
+        } else { //if not login with an anonymous user 
+            login();
+        }
+    }
+
+
+    // get the user ID from the cached currentUser object
+    private void startWithCurrentUser() {
+        //TODO
+    }
+
+    void login(){
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Anonymous login failed: " + e);
+                }
+            }
+        });
+
+    }
+}
