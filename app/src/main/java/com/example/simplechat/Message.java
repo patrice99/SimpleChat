da3ce88ce6255b2b704ec5ffc0cbc98987a221b4
Patrice Gill <pgill30@fb.com>
2020-07-06 13:34:16 -0400

    Added a Message model class

---
+++ app/src/main/java/com/example/simplechat/Message.java
@@ -0,0 +1,24 @@
+package com.example.simplechat;
+
+import com.parse.ParseObject;
+
+public class Message extends ParseObject {
+    //This model class will provide message data for the RecyclerView and will be used to retrieve and save messages to Parse 
+    public static final String USER_ID_KEY = "userId";
+    public static final String BODY_KEY = "body";
+
+    public String getUserIdKey() {
+        return getString(USER_ID_KEY);
+    }
+
+    public String getBodyKey() {
+        return getString(BODY_KEY);
+    }
+
+    public void setUserId(String userId){
+        put(USER_ID_KEY, userId);
+    }
+    public void setBody(String body){
+        put(BODY_KEY, body);
+    }
+}
